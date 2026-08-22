6e046ac4061e0cea5b4e597a503833f116a7852f
Chatnarong Thongchai <chatnarong.thongchai@gmail.com>
2026-08-22 15:22:03 +0700

    chore: reorder probe module after swagger in main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -21,13 +21,13 @@ func main() {
 		},
 		Imports: []internal.Module{
 			database.NewModule(),
-			probe.NewModule(),
 			swagger.NewModule(swagger.ModuleOptions{
 				Spec:        openAPISpec,
 				Title:       "Go with Gin and Zerolog API",
 				Version:     "0.1.0",
 				Description: "HTTP API documentation.",
 			}),
+			probe.NewModule(),
 		},
 	})
 
